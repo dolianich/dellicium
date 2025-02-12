a7474a626d59538bdb452d2ce32ca88321a053dc
CreatorMeow <daniildolia240@gmail.com>
2025-02-12 12:30:10 +0300

    Created Hide component

--- src/components/ProfileCard/Actions/Hide/Hide.tsx
+++ src/components/ProfileCard/Actions/Hide/Hide.tsx
@@ -2,10 +2,14 @@ import styles from './Hide.module.css';
 
 const Hide = () => {
   return (
-    <div>
-      
+    <div className={styles.wrapper}>
+      <div className={styles.card}>
+        <p className={styles.text}>
+          Adopt this Creator <br /> to access the actions section
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hide
+export default Hide;
