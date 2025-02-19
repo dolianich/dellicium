0e78db32b9bb5cffe347ad42112ce298e74eb54e
CreatorMeow <daniildolia240@gmail.com>
2025-02-19 14:46:32 +0300

    Added hide mode for actions section

--- src/components/ProfileCard/Actions/Actions.tsx
+++ src/components/ProfileCard/Actions/Actions.tsx
@@ -2,18 +2,31 @@ import styles from './Actions.module.css';
 import DonateBtn from './DonateBtn/DonateBtn';
 import ActionBtn from './ActionBtn/ActionBtn';
 import ActionBtnSm from './ActionBtnSm/ActionBtnSm';
-
+import { CaretUp } from '@phosphor-icons/react';
+import { useState } from 'react';
 
 interface Props {
   tips: () => void;
   gift: () => void;
 }
 
-const Actions = ({ tips, gift,}: Props) => {
+const Actions = ({ tips, gift }: Props) => {
+  const [visible, setVisible] = useState(true);
+
   return (
     <div className={styles.wrapper}>
-      <h2 className={styles.actions}>actions</h2>
-      <div className={styles.section}>
+      <div
+        className={styles.actionsDropdown}
+        onClick={() => setVisible(!visible)}
+      >
+        <h2 className={styles.actions}>actions</h2>
+        <CaretUp
+          size={20}
+          weight="regular"
+          className={visible ? styles.arrowIcon : styles.arrowIconDown}
+        />
+      </div>
+      <div className={visible ? styles.section : styles.sectionHide}>
         <div className={styles.donations}>
           <DonateBtn onClick={tips} type="tips" />{' '}
           <DonateBtn onClick={gift} type="gift" />
