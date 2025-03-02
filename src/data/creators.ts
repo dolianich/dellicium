ffdb886d711991246762d2e8547b51acf5c5fa08
CreatorMeow <daniildolia240@gmail.com>
2025-03-02 17:19:50 +0300

    Created a brand new profile

--- src/data/creators.ts
+++ src/data/creators.ts
@@ -6,6 +6,7 @@ import lucaslights from '../storage/lucaslights.jpg';
 import miainmotion from '../storage/miainmotion.jpg';
 import noahontheego from '../storage/noahonthego.jpg';
 import sophiaspark from '../storage/sophiaspark.jpg';
+import tosha_avatar from '../storage/tosha/tosha_avatar.jpg'
 
 import testVideo from '../storage/posts/testVideo.MOV';
 import second from '../storage/posts/101.png';
@@ -16,7 +17,114 @@ import sixth from '../storage/posts/105.png';
 import seventh from '../storage/posts/106.png'
 import eighth from '../storage/posts/107.png';
 
+import bite from '../storage/tosha/posts/bite.mp4';
+import dreams from '../storage/tosha/posts/dreams.jpg';
+import apple from '../storage/tosha/posts/apple.mp4';
+import smile from '../storage/tosha/posts/smile.jpg';
+import handsome from '../storage/tosha/posts/handsome.jpg';
+import freeze from '../storage/tosha/posts/freeze.jpg';
+import hunt from '../storage/tosha/posts/hunt.jpg';
+import scream from '../storage/tosha/posts/scream.jpg';
+
 const creators = [
+  {
+    id: 420,
+    username: '@tosha',
+    name: 'Tosha the cat',
+    bio: 'The Abyssinian Cat Living Its Best Life 🐾',
+    tags: ['design', 'art', 'fitness'],
+    avatar: tosha_avatar,
+    level: 1,
+    xp: 10,
+    socials: {
+      website: 'https://www.google.com/',
+      ig: 'https://www.instagram.com/',
+      x: 'https://x.com/'
+    },
+    posts: [
+      {
+        id: 100,
+        date: '25.05.2025',
+        createdAt: '2025-02-26T09:00:00Z',
+        description: 'Watch as I bite the hand that feeds me – because even the best life needs a little mischief!',
+        mediaLink: bite,
+        likes: 10000,
+        comments: 2000,
+        levelRequired: 1,
+        contentType: 'video'
+      },
+      {
+        id: 101,
+        createdAt: '2023-10-02T14:30:00Z',
+        description: 'Shine bright dream big ✨',
+        mediaLink: dreams,
+        likes: 123,
+        comments: 123,
+        levelRequired: 0,
+        contentType: 'image'
+      },
+      {
+        id: 102,
+        createdAt: '2023-10-02T14:30:00Z',
+        description: 'An apple a day keeps the vet away?',
+        mediaLink: apple,
+        likes: 123,
+        comments: 123,
+        levelRequired: 3,
+        contentType: 'video'
+      },
+      {
+        id: 103,
+        createdAt: '2023-10-02T14:30:00Z',
+        description: 'Sunshine Smiles',
+        mediaLink: smile,
+        likes: 123,
+        comments: 123,
+        levelRequired: 0,
+        contentType: 'image'
+      },
+      {
+        id: 104,
+        createdAt: '2023-10-02T14:30:00Z',
+        description: 'Look who’s stealing the spotlight today – your favorite me',
+        mediaLink: handsome,
+        likes: 123,
+        comments: 123,
+        levelRequired: 2,
+        contentType: 'image'
+      },
+      {
+        id: 105,
+        createdAt: '2023-10-02T14:30:00Z',
+        description: 'Catch me freezing in style',
+        mediaLink: freeze,
+        likes: 123,
+        comments: 123,
+        levelRequired: 1,
+        contentType: 'image'
+      },
+      {
+        id: 106,
+        createdAt: '2023-10-02T14:30:00Z',
+        description: 'Stealth Mode: Activated',
+        mediaLink: hunt,
+        likes: 123,
+        comments: 123,
+        levelRequired: 3,
+        contentType: 'image'
+      },
+      {
+        id: 107,
+        createdAt: '2023-10-02T14:30:00Z',
+        description: 'When you’re living your best life, sometimes you just have to scream it to the world!',
+        mediaLink: scream,
+        likes: 123,
+        comments: 123,
+        levelRequired: 2,
+        contentType: 'image'
+      },
+    ]
+  },
     {
       id: 1,
       username: '@elenathestar',
