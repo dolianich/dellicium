988c16e348cda3fd3b6bf1f11a81f1a7c5ed24c5
CreatorMeow <daniildolia240@gmail.com>
2025-03-05 14:13:41 +0300

    Created styles for controls

--- src/components/Post/Media/VideoControls/VideoControls.tsx
+++ src/components/Post/Media/VideoControls/VideoControls.tsx
@@ -1,5 +1,13 @@
 import styles from './VideoControls.module.css';
-import { Play, Pause } from '@phosphor-icons/react';
+import {
+  Play,
+  Pause,
+  CornersOut,
+  CornersIn,
+  SpeakerHigh,
+  SpeakerSlash,
+} from '@phosphor-icons/react';
+import { formatTime } from '../../../../utils/formatTime';
 
 interface Props {
   handlePlay?: () => void;
@@ -24,24 +32,55 @@ const VideoControls = ({
   playedSeconds,
   handleProgressTrack,
 }: Props) => {
+  const progress =
+    loadedSeconds! > 0 ? (playedSeconds! / loadedSeconds!) * 100 : 0;
+
   return (
     <>
       <button className={styles.playBtn} onClick={handlePlay}>
-        {playing ? <Pause weight='fill' size={20}/> : <Play weight='fill' size={20}/>}
+        {playing ? (
+          <Pause weight="fill" size={20} />
+        ) : (
+          <Play weight="fill" size={20} />
+        )}
       </button>
       <div className={styles.controls}>
-        <button onClick={handleScreenSize}>{fullScreen ? 'H' : 'S'}</button>
-        <button onClick={handleMute}>{mute ? 'unmute' : 'mute'}</button>
         <input
           type="range"
           min={0}
           max={loadedSeconds}
           value={playedSeconds}
-          step={1}
+          step={0.000001}
           onChange={(e) => handleProgressTrack(e.target.value)}
+          className={styles.progress}
+          style={{
+            background: `linear-gradient(to right, #BBFCDC ${progress}%, #111111 ${progress}%)`,
+          }}
         />
-        <p>{playedSeconds}</p>
-        <p>{loadedSeconds}</p>
+        <div className={styles.bottom}>
+          <div className={styles.duration}>
+            <p className={styles.playedSeconds}>{formatTime(playedSeconds!)}</p>
+            <p className={styles.playedSeconds}>/</p>
+            <p className={styles.loadedSeconds}>{formatTime(loadedSeconds!)}</p>
+          </div>
+
+          <div className={styles.topControls}>
+            <button onClick={handleMute} className={styles.controlBtn}>
+              {mute ? (
+                <SpeakerSlash weight="regular" size={16} />
+              ) : (
+                <SpeakerHigh weight="regular" size={16} />
+              )}
+            </button>
+            <button onClick={handleScreenSize} className={styles.controlBtn}>
+              {fullScreen ? (
+                <CornersIn weight="regular" size={16} />
+              ) : (
+                <CornersOut weight="regular" size={16} />
+              )}
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
