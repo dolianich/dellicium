df370575149e74bd9ae79565bbeabe7868455ab3
CreatorMeow <daniildolia240@gmail.com>
2025-03-18 15:50:39 +0300

    Added back button and useNavigate to the wishlist

--- src/pages/WishlistPage.tsx
+++ src/pages/WishlistPage.tsx
@@ -1,12 +1,20 @@
 import creators from '../data/creators';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const WishlistPage = () => {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate(-1);
+  };
   const params = useParams<{ creatorId: string }>();
   const creator = creators.find(
     (creator) => creator.username === params.creatorId
   );
-  return <div>Wishlist {creator?.name}</div>;
+  return (
+    <div>
+      <button onClick={handleNavigate}>Back</button> Wishlist {creator?.name}
+    </div>
+  );
 };
 
 export default WishlistPage;
