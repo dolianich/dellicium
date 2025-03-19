342cfb609274ea5492af172127c16dab41a0c478
CreatorMeow <daniildolia240@gmail.com>
2025-03-19 16:28:03 +0300

    Added wishes data for dynamic rendering

--- src/components/WishCard/WishCard.tsx
+++ src/components/WishCard/WishCard.tsx
@@ -1,22 +1,28 @@
 import styles from './WishCard.module.css';
-import feeder from '../../storage/wishes/feeder.png';
 import Button from './Button/Button';
 
-const WishCard = () => {
+interface Props {
+  img?: string;
+  title: string;
+  description?: string;
+  price: number;
+}
+
+const WishCard = ({ img, title, description, price }: Props) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.imgContainer}>
-        <img src={feeder} alt="img" className={styles.img} />
+        <img src={img} alt="img" className={styles.img} />
       </div>
       <div className={styles.info}>
         <div className={styles.top}>
-          <h4 className={styles.title}>Wish name</h4>
-          <p className={styles.description}>Wish description</p>
+          <h4 className={styles.title}>{title}</h4>
+          <p className={styles.description}>{description}</p>
         </div>
         <div className={styles.bottom}>
-          <h3 className={styles.price}>100 SOL</h3>
+          <h3 className={styles.price}>{price} SOL</h3>
           <div className={styles.btns}>
-            <Button title='Fund Dream'/>
+            <Button title="Fund Dream" />
           </div>
         </div>
       </div>
--- src/pages/WishlistPage.tsx
+++ src/pages/WishlistPage.tsx
@@ -1,6 +1,11 @@
 import creators from '../data/creators';
 import { useParams, useNavigate } from 'react-router-dom';
 import WishCard from '../components/WishCard/WishCard';
+import bed from '.././storage/wishes/bed.png';
+import feeder from '.././storage/wishes/feeder.png';
+import laser from '.././storage/wishes/laser.png';
+import tree from '.././storage/wishes/tree.png';
+import tuna from '.././storage/wishes/tuna.png';
 
 const WishlistPage = () => {
   const navigate = useNavigate();
@@ -11,10 +16,55 @@ const WishlistPage = () => {
   const creator = creators.find(
     (creator) => creator.username === params.creatorId
   );
+
+  const wishes = [
+    {
+      img: bed,
+      title: 'Heated Cat Bed',
+      description:
+        'A plush, self-warming bed for the ultimate cat nap experience',
+      price: 1.2,
+    },
+    {
+      img: feeder,
+      title: 'Personal Cat Butler',
+      description:
+        'A robotic feeder and treat dispenser that serves your cat on demand',
+      price: 3.5,
+    },
+    {
+      img: laser,
+      title: 'Smart Laser Toy',
+      description:
+        'An automated laser toy that keeps your cat entertained for hours',
+      price: 0.9,
+    },
+    {
+      img: tree,
+      title: 'Luxury Cat Tree',
+      description:
+        'A towering cat tree with scratching posts, hammocks, and a cozy hideout',
+      price: 2.5,
+    },
+    {
+      img: tuna,
+      title: 'Gourmet Tuna Feast',
+      description:
+        'A year supply of premium, sushi-grade tuna for the fanciest of feline palates',
+      price: 1.8,
+    },
+  ];
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <button onClick={handleNavigate}>Back</button> Wishlist {creator?.name}
-      <WishCard />
+      {wishes.map((wish) => (
+        <WishCard
+          title={wish.title}
+          img={wish.img}
+          description={wish.description}
+          price={wish.price}
+        />
+      ))}
     </div>
   );
 };
