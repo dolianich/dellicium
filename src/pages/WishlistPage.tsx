f2a402db7193f3cc4ad8db9cdcef6aadb045ed0e
CreatorMeow <daniildolia240@gmail.com>
2025-03-20 13:46:24 +0300

    Created a separate component for Wishes grid

--- src/pages/WishlistPage.tsx
+++ src/pages/WishlistPage.tsx
@@ -1,11 +1,7 @@
+import styles from './styles/Page.module.css';
 import creators from '../data/creators';
 import { useParams, useNavigate } from 'react-router-dom';
-import WishCard from '../components/WishCard/WishCard';
-import bed from '.././storage/wishes/bed.png';
-import feeder from '.././storage/wishes/feeder.png';
-import laser from '.././storage/wishes/laser.png';
-import tree from '.././storage/wishes/tree.png';
-import tuna from '.././storage/wishes/tuna.png';
+import WishesGrid from '../components/WishesGrid/WishesGrid';
 
 const WishlistPage = () => {
   const navigate = useNavigate();
@@ -17,54 +13,10 @@ const WishlistPage = () => {
     (creator) => creator.username === params.creatorId
   );
 
-  const wishes = [
-    {
-      img: bed,
-      title: 'Heated Cat Bed',
-      description:
-        'A plush, self-warming bed for the ultimate cat nap experience',
-      price: 1.2,
-    },
-    {
-      img: feeder,
-      title: 'Personal Cat Butler',
-      description:
-        'A robotic feeder and treat dispenser that serves your cat on demand',
-      price: 3.5,
-    },
-    {
-      img: laser,
-      title: 'Smart Laser Toy',
-      description:
-        'An automated laser toy that keeps your cat entertained for hours',
-      price: 0.9,
-    },
-    {
-      img: tree,
-      title: 'Luxury Cat Tree',
-      description:
-        'A towering cat tree with scratching posts, hammocks, and a cozy hideout',
-      price: 2.5,
-    },
-    {
-      img: tuna,
-      title: 'Gourmet Tuna Feast',
-      description:
-        'A year supply of premium, sushi-grade tuna for the fanciest of feline palates',
-      price: 1.8,
-    },
-  ];
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <div className={styles.container}>
       <button onClick={handleNavigate}>Back</button> Wishlist {creator?.name}
-      {wishes.map((wish) => (
-        <WishCard
-          title={wish.title}
-          img={wish.img}
-          description={wish.description}
-          price={wish.price}
-        />
-      ))}
+      <WishesGrid />
     </div>
   );
 };
