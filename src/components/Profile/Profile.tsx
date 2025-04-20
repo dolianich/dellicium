0e1f722d7efae34c02abe5e0ecb8799c0efbbc83
CreatorMeow <daniildolia240@gmail.com>
2025-04-20 18:25:40 +0300

    Created custom design for Wallet connecting button

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,10 +1,31 @@
 import styles from './Profile.module.css';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { useRef } from 'react';
 
 const Profile = () => {
+  const { connected, publicKey } = useWallet();
+  const hiddenButtonRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = () => {
+    const walletButton = hiddenButtonRef.current?.querySelector('button');
+    if (walletButton instanceof HTMLButtonElement) {
+      walletButton.click();
+    }
+  };
+
   return (
-    <div>
-      <WalletMultiButton />
+    <div className={styles.wrapper}>
+      <div className={styles.hiddenButton} ref={hiddenButtonRef}>
+        <WalletMultiButton />
+      </div>
+      <button onClick={handleClick} className={styles.connectButton}>
+        {connected
+          ? `${publicKey?.toString().slice(0, 4)}...${publicKey
+              ?.toString()
+              .slice(-4)}`
+          : 'Connect Wallet'}
+      </button>
     </div>
   );
 };
