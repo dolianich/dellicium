48e00de0e8216321afe4c002d5a049a090e01bab
CreatorMeow <daniildolia240@gmail.com>
2025-04-20 17:25:48 +0300

    Moved wallet context provider to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -14,43 +14,30 @@ import NotFoundPage from './pages/NotFoundPage';
 import CreatorPage from './pages/CreatorPage';
 import WishlistPage from './pages/WishlistPage';
 import MobNav from './components/MobNav/MobNav';
-import { useMemo } from 'react';
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from '@solana/wallet-adapter-react';
-import { clusterApiUrl } from '@solana/web3.js';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import WalletContextProvider from './components/WalletContextProvider/WalletContextProvider';
 
 function App() {
-  const endpoint = clusterApiUrl('devnet');
-  const wallets = useMemo(() => [], []);
-
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets}>
-        <WalletModalProvider>
-          <Router>
-            <SideBar />
-            <MobNav />
-            <Routes>
-              <Route path="/" element={<Navigate to="discover" />} />
-              <Route path="/discover" Component={DiscoverPage} />
-              <Route path="*" Component={NotFoundPage} />
-              <Route path="/assets" Component={AssetsPage} />
-              <Route path="/market" Component={MarketPage} />
-              <Route path="/creators" Component={CreatorsPage} />
-              <Route path="/notifications" Component={NotificationsPage} />
-              <Route path="/creators/:creatorId" element={<CreatorPage />} />
-              <Route
-                path="/creators/:creatorId/wishlist"
-                element={<WishlistPage />}
-              />
-            </Routes>
-          </Router>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <WalletContextProvider>
+    <Router>
+      <SideBar />
+      <MobNav />
+      <Routes>
+        <Route path="/" element={<Navigate to="discover" />} />
+        <Route path="/discover" Component={DiscoverPage} />
+        <Route path="*" Component={NotFoundPage} />
+        <Route path="/assets" Component={AssetsPage} />
+        <Route path="/market" Component={MarketPage} />
+        <Route path="/creators" Component={CreatorsPage} />
+        <Route path="/notifications" Component={NotificationsPage} />
+        <Route path="/creators/:creatorId" element={<CreatorPage />} />
+        <Route
+          path="/creators/:creatorId/wishlist"
+          element={<WishlistPage />}
+        />
+      </Routes>
+    </Router>
+   </WalletContextProvider>
   );
 }
 
---
+++ src/components/WalletContextProvider/WalletContextProvider.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, useMemo } from 'react';
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+interface Props {
+  children: ReactNode;
+}
+
+const WalletContextProvider = ({ children }: Props) => {
+  const endpoint = clusterApiUrl('devnet');
+  const wallets = useMemo(() => [], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
