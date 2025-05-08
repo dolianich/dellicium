45c4489ae02d07b1a13ed60010797f425382375e
CreatorMeow <daniildolia240@gmail.com>
2025-05-08 17:57:33 +0300

    Created a test script, ran in and created test user data account

--- solana/creator_profile/tests/creator_profile.ts
+++ solana/creator_profile/tests/creator_profile.ts
@@ -1,16 +1,31 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { PublicKey, SystemProgram } from '@solana/web3.js';
 import { CreatorProfile } from "../target/types/creator_profile";
 
 describe("creator_profile", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.creatorProfile as Program<CreatorProfile>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const program = anchor.workspace.CreatorProfile as Program<CreatorProfile>;
+
+  it("Creates profile", async () => {
+    const [profilePDA] = await PublicKey.findProgramAddress(
+      [Buffer.from("profile"), provider.wallet.publicKey.toBuffer()],
+      program.programId
+    );
+    console.log("PROFILE PDA:", profilePDA.toString());
+
+    await program.methods.createProfile(
+      "test_userName",
+      "Test Name",
+      "Test bio",
+      ["tag_example1", "tag_example2"]
+    ).accounts({
+      profile: profilePDA,
+      authority: provider.wallet.publicKey,
+      system_program: SystemProgram.programId
+    }).rpc();
   });
 });
